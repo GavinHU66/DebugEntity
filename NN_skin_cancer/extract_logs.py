1eb0b3c082d445492be42de277f4f9353bdd2a90
Edison Ni <EdisonNi-hku>
2021-03-05 21:29:56 +0800

    extract script adding

--- NN_skin_cancer/extract_logs.py
+++ NN_skin_cancer/extract_logs.py
@@ -10,12 +10,12 @@ for ex in experiments:
     ex_path = os.path.join("./" + result_folder, ex)
     os.mkdir(ex_path)
     log_path = os.path.join(models, 'train_' + ex + '.log')
-    os.system('copy %s %s' % (log_path, ex_path))
+    os.system('cp %s %s' % (log_path, ex_path))
     for dir in os.listdir(os.path.join(models, 'model_' + ex)):
         if dir.startswith("fold"):
             tgt1 = os.path.join(models, 'model_' + ex, dir, 'progression_valInd.mat')
             tgt2 = os.path.join(models, 'model_' + ex, dir, "2020.ham" + ex, 'model.pkl')
             dest = os.path.join(ex_path, dir)
             os.mkdir(dest)
-            os.system('copy %s %s' % (tgt1, dest))
-            os.system('copy %s %s' % (tgt2, dest))
+            os.system('cp %s %s' % (tgt1, dest))
+            os.system('cp %s %s' % (tgt2, dest))
