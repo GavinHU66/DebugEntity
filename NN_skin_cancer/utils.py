4f2db19237cdc98c95682636ebe0556b82332ba7
Edison Ni <EdisonNi-hku>
2021-04-08 22:09:50 +0800

    meta aug

--- NN_skin_cancer/utils.py
+++ NN_skin_cancer/utils.py
@@ -310,6 +310,141 @@ class HAMDataset(Dataset):
         return (x, meta_vector), y, idx
 
 
+class MetaAugDataset(Dataset):
+    """HAM10000 dataset."""
+
+    def __init__(self, mdlParams, indSet, index=None):
+        """
+        Args:
+            mdlParams (dict): Configuration for loading
+            indSet (string): Indicates train, val, test
+        """
+        # Mdlparams
+        self.mdlParams = mdlParams
+        # Current indSet = 'trainInd'/'valInd'/'testInd'
+        self.indSet = indSet
+        if self.indSet == 'trainInd':
+            self.root = mdlParams['dataDir'] + '/train'
+            self.index = index
+        elif self.indSet == 'valInd':
+            self.root = mdlParams['dataDir'] + '/train'
+            self.index = index
+        else:
+            self.root = mdlParams['dataDir'] + '/test'
+        self.names_list = []
+        # Number of classes
+        self.numClasses = mdlParams['numClasses']
+        # Size to crop
+        self.crop_size = mdlParams['crop_size']
+        # Model input size
+        self.input_size = (np.int32(mdlParams['input_size'][0]), np.int32(mdlParams['input_size'][1]))
+        # Potential class balancing option
+        self.balancing = mdlParams['balance_classes']
+        # Potential setMean and setStd
+        self.setMean = mdlParams['setMean'].astype(np.float32)
+        self.setStd = mdlParams['setStd'].astype(np.float32)
+        # Only downsample
+        self.only_downsmaple = mdlParams.get('only_downsmaple', True)
+        # Meta csv
+        self.meta_path = mdlParams['meta_path']
+        self.meta_df = pd.read_pickle(self.meta_path)
+
+        self.subsets_size = [0, 0, 0, 0, 0, 0, 0]
+        self.image_path = []
+        self.image_labels = []
+        self.image_meta = []
+        for img in os.listdir(self.root):
+            id = img.split('.')[0]
+            label = list(self.meta_df.loc[self.meta_df['image_id'] == id, 'dx'])[0]
+            self.image_labels.append(CLASS_LABELS.index(label))
+            self.subsets_size[CLASS_LABELS.index(label)] += 1
+            self.image_meta.append(self.meta_df.loc[self.meta_df['image_id'] == id, self.mdlParams['meta_features']].to_numpy())
+            self.image_path.append(os.path.join(self.root, img))
+        self.image_path = np.asarray(self.image_path)
+        self.image_labels = np.asarray(self.image_labels)
+        self.image_meta = np.asarray(self.image_meta)
+
+        if indSet == 'trainInd':
+            if index is not None:
+                self.image_path = self.image_path[index]
+                self.image_labels = self.image_labels[index]
+                self.image_meta = self.image_meta[index]
+            all_transforms = []
+            if self.only_downsmaple:
+                all_transforms.append(transforms.Resize(self.input_size))
+            else:
+                all_transforms.append(transforms.Resize(self.crop_size))
+                all_transforms.append(transforms.RandomCrop(self.input_size))
+            if mdlParams.get('flip_lr_ud', False):
+                all_transforms.append(transforms.RandomHorizontalFlip())
+                all_transforms.append(transforms.RandomVerticalFlip())
+            # Full rot
+            if mdlParams.get('full_rot', 0) > 0:
+                if mdlParams.get('scale', False):
+                    all_transforms.append(transforms.RandomChoice([transforms.RandomAffine(mdlParams['full_rot'], scale=mdlParams['scale'], shear=mdlParams.get('shear', 0), resample=Image.NEAREST),
+                                                                   transforms.RandomAffine(mdlParams['full_rot'], scale=mdlParams['scale'], shear=mdlParams.get('shear', 0), resample=Image.BICUBIC),
+                                                                   transforms.RandomAffine(mdlParams['full_rot'], scale=mdlParams['scale'], shear=mdlParams.get('shear', 0), resample=Image.BILINEAR)]))
+                else:
+                    all_transforms.append(transforms.RandomChoice(
+                        [transforms.RandomRotation(mdlParams['full_rot'], resample=Image.NEAREST),
+                         transforms.RandomRotation(mdlParams['full_rot'], resample=Image.BICUBIC),
+                         transforms.RandomRotation(mdlParams['full_rot'], resample=Image.BILINEAR)]))
+            # Color distortion
+            if mdlParams.get('full_color_distort') is not None:
+                all_transforms.append(transforms.ColorJitter(brightness=mdlParams.get('brightness_aug', 32. / 255.),
+                                                             saturation=mdlParams.get('saturation_aug', 0.5),
+                                                             contrast=mdlParams.get('contrast_aug', 0.5),
+                                                             hue=mdlParams.get('hue_aug', 0.2)))
+            else:
+                all_transforms.append(transforms.ColorJitter(brightness=32. / 255., saturation=0.5))
+            # Autoaugment
+            if self.mdlParams.get('randaugment', False):
+                all_transforms.append(RandAugment(self.mdlParams.get('N'), self.mdlParams.get('M')))
+            # Cutout
+            if self.mdlParams.get('cutout', 0) > 0:
+                all_transforms.append(Cutout_v0(n_holes=1, length=self.mdlParams['cutout']))
+            # Normalize
+            all_transforms.append(transforms.ToTensor())
+            all_transforms.append(
+                transforms.Normalize(np.float32(self.mdlParams['setMean']), np.float32(self.mdlParams['setStd'])))
+            # All transforms
+            self.composed = transforms.Compose(all_transforms)
+        else:
+            if index is not None:
+                self.image_path = self.image_path[index]
+                self.image_labels = self.image_labels[index]
+                self.image_meta = self.image_meta[index]
+            self.composed = transforms.Compose([
+                transforms.Resize(self.input_size),
+                transforms.ToTensor(),
+                transforms.Normalize(torch.from_numpy(self.setMean).float(),
+                                     torch.from_numpy(self.setStd).float())
+                ])
+
+    def __len__(self):
+        return len(self.image_path)
+
+    def __getitem__(self, idx):
+        path = self.image_path[idx]
+        x = Image.open(path)
+        y = self.image_labels[idx]
+        x_meta = self.image_meta[idx]
+
+        # Apply
+        x = self.composed(x)
+        if self.mdlParams.get('with_meta', False) and self.indSet == 'trainInd':
+            if torch.rand(1) < self.mdlParams['drop_augment']:
+                if 'age' in self.mdlParams['meta_types']:
+                    x_meta[0:18] = np.zeros([18])
+            if torch.rand(1) < self.mdlParams['drop_augment']:
+                if 'sex' in self.mdlParams['meta_types']:
+                    x_meta[18:21] = np.zeros([3])
+            if torch.rand(1) < self.mdlParams['drop_augment']:
+                if 'location' in self.mdlParams['meta_features']:
+                    x_meta[21:36] = np.zeros([15])
+        return (x, x_meta), y, idx
+
+
 class BinaryDataset(Dataset):
     """HAM10000 binary dataset."""
 
